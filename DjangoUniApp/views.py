3a4a6d6f07c2f31a45607ca1b5d7486bdab5006d
XxxAqueousxxX <trujillorojas88@gmail.com>
2022-06-24 01:40:33 -0500

    Update views.py

--- DjangoUniApp/views.py
+++ DjangoUniApp/views.py
@@ -1,8 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Login")
+    context = {'clase' : 'Aprendiendo Django'}
+    return HttpResponse("Login.html", context)
 
 
 
