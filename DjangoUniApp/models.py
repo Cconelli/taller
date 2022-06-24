2c808e5aa170cf3ee8cb41d91935ce853953dbc6
XxxAqueousxxX <trujillorojas88@gmail.com>
2022-06-24 01:40:23 -0500

    Update models.py

--- DjangoUniApp/models.py
+++ DjangoUniApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    email = models.EmailField()
+    
