3b30703ea753d6e5afedca54c69f40ca8e856df6
XxxAqueousxxX <trujillorojas88@gmail.com>
2022-06-24 01:25:28 -0500

    Update views.py

--- DjangoUniApp/views.py
+++ DjangoUniApp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Listado de modulos")
+    return HttpResponse("Login")
 
 
 
